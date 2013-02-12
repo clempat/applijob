3a753cfa4809192a7f7cc7a6afb476741fd9e02a
clempat <clement.patout@gmail.com>
2013-02-12 22:47:14 +0100

    rails_base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Applijob::Application.config.secret_token = '52f4c11bae25b7cb54d2656ee58244f1d7a749b51f10b82ae57395aab4e694618d2f55205fd40300cb6638c65f60adaf512792cc92e07d480e7d38253d033971'
